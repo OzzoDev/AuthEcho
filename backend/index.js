56a59318cd52e7090afd24fe22da97e5555378c3
oscar <oscar.burman005@gmail.com>
2024-12-31 08:56:13 +0100

    Server set up to run

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/ping", (_, res) => {
+  res.send("PONG");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
