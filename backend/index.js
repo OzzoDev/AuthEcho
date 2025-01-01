1cb20a97120336c577abea38bb517f1927dd55c9
oscar <oscar.burman005@gmail.com>
2025-01-01 21:03:19 +0100

    Connected to mongodb database and created the user model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+require("./modules/db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
---
+++ backend/modules/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+
+module.exports = UserModel;
