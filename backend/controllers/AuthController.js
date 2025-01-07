3e0c94b44eed2863f6784887853a96731b0fa52b
oscar <oscar.burman005@gmail.com>
2025-01-07 18:12:26 +0100

    Refactored the code for the update email function and created a simlair function to update username

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -52,17 +52,50 @@ const signin = async (req, res) => {
 
 const updateEmail = async (req, res) => {
   try {
-    const { name, email: newEmail } = req.body;
+    const { userData, email: newEmail } = req.body;
     const { email: currentEmail } = req.user;
 
-    const user = await UserModel.findOne({ $or: [{ email: currentEmail }, { name }] });
+    const user = await UserModel.findOne({ $or: [{ email: userData }, { name: userData }] });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found", success: false });
+    }
+
+    if (newEmail && newEmail === currentEmail) {
+      return res.status(409).json({ message: "New email must be different from the current email", success: false });
+    }
+
+    if (newEmail) {
+      user.email = newEmail;
+      await user.save();
+    }
+
+    res.status(200).json({ message: "Email updated successfully", success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", success: false });
+  }
+};
+
+const updateUsername = async (req, res) => {
+  try {
+    const { userData, name: newName } = req.body;
+    const { name: currentName } = req.user;
+
+    const user = await UserModel.findOne({ $or: [{ email: userData }, { name: userData }] });
 
     if (!user) {
       return res.status.json({ message: "User not found", success: false });
     }
 
-    user.email = newEmail;
-    await user.save();
+    if (newName && newName === currentName) {
+      return res.status(409).json({ message: "New username must be different from the current username", success: false });
+    }
+
+    if (newName) {
+      user.name = newName;
+      await user.save();
+    }
 
     res.status(200).json({ message: "Email updated successfully", success: true });
   } catch (error) {
