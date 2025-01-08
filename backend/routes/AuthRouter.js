e117549191ed02738f71aa5d3a08d87c36738043
oscar <oscar.burman005@gmail.com>
2025-01-08 14:19:14 +0100

    Extracted files to root folder

--- backend/routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-const { signup, signin } = require("../controllers/AuthController");
-const { signupValidation, signinValidation } = require("../middlewares/AuthValidation");
-
-const router = require("express").Router();
-
-router.post("/signin", signinValidation, signin);
-router.post("/signup", signupValidation, signup);
-
-module.exports = router;
