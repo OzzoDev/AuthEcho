6a6dfd0b3637333e182cbe8fcd8a205703412071
oscar <oscar.burman005@gmail.com>
2025-01-09 17:21:56 +0100

    Log out function that clears cookies and redirects to signin page

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,13 +1,16 @@
 //@ts-ignore
 import "../styles/accountPage.css";
 import { useEffect, useState } from "react";
-import { getData, verifyAccount } from "../utils/utils";
+import { getData, removeData, removeToken, verifyAccount } from "../utils/utils";
 import { USEREMAIL_KEY, USERNAME_KEY } from "../constants/contants";
+import { useNavigate } from "react-router-dom";
 
 export default function AccountPage() {
   const [name, setName] = useState<string>(getData(USERNAME_KEY));
   const [email, setEmail] = useState<string>(getData(USEREMAIL_KEY));
   const [error, setError] = useState<string>("");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const checkVerification = async () => {
@@ -17,15 +20,23 @@ export default function AccountPage() {
     checkVerification();
   }, []);
 
+  const logOut = () => {
+    removeData(USERNAME_KEY);
+    removeData(USEREMAIL_KEY);
+    removeToken();
+    navigate("/signin");
+  };
+
   return (
     <>
       {error ? (
         <h1 className="error">{error}</h1>
       ) : (
-        <div>
+        <div className="accountContainer">
           <h1>Your are signed in!</h1>
           <p>Username: {name}</p>
           <p>Email: {email}</p>
+          <button onClick={logOut}>Log out</button>
         </div>
       )}
     </>
