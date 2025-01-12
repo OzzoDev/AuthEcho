fa5a937dcb8a196a7c1e5d2bf3ad86c98fad6225
oscar <oscar.burman005@gmail.com>
2025-01-12 23:16:09 +0100

    Refactored reset password page

--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -1,16 +1,15 @@
-// @ts-ignore
-import "../styles/signupPage.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FetchStatus, ResetPassword } from "../types/userTypes";
-import { capitalize, removeAllQuotes } from "../utils/utils";
-import axios from "axios";
 import { resetPassword, sendVerificationCode, validatePassword } from "../utils/ServerClient";
 import ReactLoading from "react-loading";
 import Navbar from "../components/Navbar";
+import FormPasswordInput from "../components/form/FormPasswordInput";
+import FormInput from "../components/form/FormInput";
+import FormVerify from "../components/form/FormVerify";
 
 export default function SigninPage() {
-  const [formData, setFormData] = useState<ResetPassword>({ userData: "", verificationCode: "", newPassword: "", confirmNewPassword: "" });
+  const [formData, setFormData] = useState<ResetPassword>({ userData: "", newPassword: "", confirmNewPassword: "", verificationCode: "" });
   const [verify, setVerify] = useState<boolean>(false);
   const [status, setStatus] = useState<FetchStatus>("idle");
   const [error, setError] = useState<string>("");
@@ -18,95 +17,82 @@ export default function SigninPage() {
   const navigate = useNavigate();
   const dispatch = useNavigate();
 
+  useEffect(() => {
+    const verifyAccountAsync = async () => {
+      if (formData.verificationCode.length === 8) {
+        const passwordResposne = await resetPassword(formData, setStatus, setError, dispatch);
+        if (passwordResposne) {
+          navigate("/account");
+        }
+      }
+    };
+
+    verifyAccountAsync();
+  }, [formData.verificationCode]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const handleSetVerificationCode = (verificationCode: string) => {
+    setFormData((prevData) => ({ ...prevData, ["verificationCode"]: verificationCode }));
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      setStatus("loading");
-      setError("");
-      if (verify) {
-        const passwordResposne = await resetPassword(formData, setStatus, setError, dispatch);
-        if (passwordResposne) {
-          navigate("/account");
-        }
-      } else {
-        const validatePasswordResponse = await validatePassword({ newPassword: formData.newPassword, confirmNewPassword: formData.confirmNewPassword, userData: formData.userData }, setStatus, setError);
-        if (validatePasswordResponse) {
-          const verificationCodeResponse = await sendVerificationCode({ userData: formData.userData, action: "verifyEmail" }, setStatus, setError);
-          if (verificationCodeResponse) {
-            setVerify(true);
-          }
-        }
+
+    if (verify) {
+      const passwordResposne = await resetPassword(formData, setStatus, setError, dispatch);
+      if (passwordResposne) {
+        navigate("/account");
       }
-      setStatus("success");
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const errorMessage: string = capitalize(removeAllQuotes(error.response?.data.message || error.message));
-        console.error("Password resetting failed", error);
-        setStatus("error");
-        setError(errorMessage);
+    } else {
+      const validatePasswordResponse = await validatePassword({ newPassword: formData.newPassword, confirmNewPassword: formData.confirmNewPassword, userData: formData.userData }, setStatus, setError);
+      if (validatePasswordResponse) {
+        const verificationCodeResponse = await sendVerificationCode({ userData: formData.userData, action: "verifyEmail" }, setStatus, setError);
+        if (verificationCodeResponse) {
+          setVerify(true);
+        }
       }
     }
   };
 
+  if (status === "loading") {
+    return (
+      <>
+        <ReactLoading type="spin" color="#00f" height={50} width={50} />
+        <Navbar />
+      </>
+    );
+  }
+
   return (
     <>
-      {status === "loading" ? (
-        <ReactLoading type="spin" color="#00f" height={50} width={50} />
-      ) : (
-        <>
-          <Navbar />
-          <form onSubmit={handleSubmit}>
-            {!verify ? (
-              <>
-                <div>
-                  <label>
-                    Email or Username
-                    <input type="text" name="userData" value={formData.userData} onChange={handleChange} required />
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    New password
-                    <input type="password" name="newPassword" value={formData.newPassword} onChange={handleChange} required />
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    Confirm new password
-                    <input type="password" name="confirmNewPassword" value={formData.confirmNewPassword} onChange={handleChange} required />
-                  </label>
-                </div>
-                <p className="errorMessage">{error}</p>
-                <button type="submit">Continue</button>
-                <div className="links">
-                  <a href="/signup">No account? Sign up here</a>
-                  <a href="/signin">Already have an account? Sign in here</a>
-                </div>
-              </>
-            ) : (
-              <>
-                <div>
-                  <label>
-                    Verification code
-                    <input type="text" name="verificationCode" value={formData.verificationCode} onChange={handleChange} required />
-                  </label>
-                </div>
-                <p className="errorMessage">{error}</p>
-                <button type="submit">Sign In</button>
-                <div className="links">
-                  <a href="/signup">No account? Sign up here</a>
-                  <a href="/signin">Already have an account? Sign in here</a>
-                </div>
-              </>
-            )}
-          </form>
-        </>
-      )}
+      <Navbar />
+      <h1 className="page-headline">Quickly Reset Your Password and Secure Your Account!</h1>
+      <form onSubmit={handleSubmit}>
+        {!verify ? (
+          <>
+            <h2 className="form-headline">Reset your password!</h2>
+            <FormInput labelText="Username" name="userData" value={formData.userData} onChange={handleChange} required />
+            <FormPasswordInput labelText="Password" name="newPassword" value={formData.newPassword} onChange={handleChange} required />
+            <FormPasswordInput labelText="Confirm password" name="confirmNewPassword" value={formData.confirmNewPassword} onChange={handleChange} required />
+            <button type="submit" className="submit-btn btn btn-primary">
+              Reset
+            </button>
+          </>
+        ) : (
+          <>
+            <>
+              <h2 className="form-headline">Verify Account!</h2>
+              <p className="form-info">Please check your inbox for an 8-character verification code and enter it in the field provided below</p>
+              <FormVerify setVerificationCode={handleSetVerificationCode} />
+            </>
+          </>
+        )}
+      </form>
+      <h2 className="errorMessage">{error}</h2>
     </>
   );
 }
