7a7c1c85e80f670a028b5317d4c4f208c34f7a54
oscar <oscar.burman005@gmail.com>
2025-02-03 16:22:38 +0100

    Cleaned dependencies and implemented a spinner using react-spinners

--- client/public/src/components/form/AuthForm.tsx
+++ client/public/src/components/form/AuthForm.tsx
@@ -1,4 +1,3 @@
-import ReactLoading from "react-loading";
 import useFormStore from "../../hooks/useFormStore";
 import { AUTH_HELPER } from "../../utils/authHelper";
 import Dropdown from "./Dropdown";
@@ -10,6 +9,7 @@ import { useEffect, useState } from "react";
 import { FormUsage } from "../../types/types";
 import PrimaryBtn from "../btn/PrimaryBtn";
 import ToggleBtn from "../btn/ToggleBtn";
+import { HashLoader } from "react-spinners";
 
 interface Props {
   formUsage: FormUsage;
@@ -54,64 +54,60 @@ export default function AuthForm({
     setFormData({ rememberUser: !formData.rememberUser }, "rememberUser");
   };
 
+  if (formStatus === "loading") {
+    return <HashLoader size={50} color="white" />;
+  }
+
   return (
     <>
-      {formStatus === "loading" ? (
-        <ReactLoading type="spin" color="#00f" height={50} width={50} />
-      ) : (
-        <>
-          <form
-            onSubmit={onSubmit}
-            className="flex flex-col gap-y-12 p-10 rounded-[20px] w-[90%] max-w-[560px] bg-slate-700 shadow-[0_0px_30px_rgb(255,255,255,0.5)]">
-            <h2 className="text-2xl font-semibold text-cyan-400 mb-8">{HEADLINE}</h2>
-            {SUBLINE && <p className="text-lg">{SUBLINE}</p>}
-            {DYNAMICLINE && <p className="font-semibold">{dynamicText}</p>}
-            {STATE &&
-              INPUTS?.map((formInput, index) => {
-                const type = formInput.type;
-                const labelText = formInput.labelText || "";
-                const name = formInput.name || "";
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col gap-y-12 p-10 rounded-[20px] w-[90%] max-w-[560px] bg-slate-700 shadow-[0_0px_30px_rgb(255,255,255,0.5)]">
+        <h2 className="text-2xl font-semibold text-cyan-400 mb-8">{HEADLINE}</h2>
+        {SUBLINE && <p className="text-lg">{SUBLINE}</p>}
+        {DYNAMICLINE && <p className="font-semibold">{dynamicText}</p>}
+        {STATE &&
+          INPUTS?.map((formInput, index) => {
+            const type = formInput.type;
+            const labelText = formInput.labelText || "";
+            const name = formInput.name || "";
 
-                if (!type) {
+            if (!type) {
+              return (
+                <FormInput key={index} labelText={labelText} name={name} onChange={onChange} />
+              );
+            } else {
+              switch (type) {
+                case "password":
                   return (
-                    <FormInput key={index} labelText={labelText} name={name} onChange={onChange} />
+                    <FormPasswordInput
+                      key={index}
+                      labelText={labelText}
+                      name={name}
+                      onChange={onChange}
+                    />
                   );
-                } else {
-                  switch (type) {
-                    case "password":
-                      return (
-                        <FormPasswordInput
-                          key={index}
-                          labelText={labelText}
-                          name={name}
-                          onChange={onChange}
-                        />
-                      );
-                    case "code":
-                      return <FormVerify key={index} formUsage={formUsage} />;
-                    case "select":
-                      return (
-                        <Dropdown key={index} items={dropDownItems || []} onSelect={onChange} />
-                      );
-                  }
-                }
-              })}
-            <div className="flex justify-center items-center w-full h-[60px] my-[-30px]">
-              <p className="font-semibold text-xl text-center text-red-500">{formError}</p>
-            </div>
-            {RENDERREMEMBERBTN && (
-              <ToggleBtn
-                btnText="Remember me"
-                fontSize="lg"
-                onClick={handleRemeberUser}
-                selected={formData.rememberUser}
-              />
-            )}
-            {BTNTEXT && <PrimaryBtn btnText={BTNTEXT} type="submit" width="w-full" />}
-          </form>
-          <Stepper steps={maxSteps} selectedIndex={lastestStep} />
-        </>
-      )}
+                case "code":
+                  return <FormVerify key={index} formUsage={formUsage} />;
+                case "select":
+                  return <Dropdown key={index} items={dropDownItems || []} onSelect={onChange} />;
+              }
+            }
+          })}
+        <div className="flex justify-center items-center w-full h-[60px] my-[-30px]">
+          <p className="font-semibold text-xl text-center text-red-500">{formError}</p>
+        </div>
+        {RENDERREMEMBERBTN && (
+          <ToggleBtn
+            btnText="Remember me"
+            fontSize="lg"
+            onClick={handleRemeberUser}
+            selected={formData.rememberUser}
+          />
+        )}
+        {BTNTEXT && <PrimaryBtn btnText={BTNTEXT} type="submit" width="w-full" />}
+      </form>
+      <Stepper steps={maxSteps} selectedIndex={lastestStep} />
     </>
   );
 }
